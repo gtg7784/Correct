16d6b43fef0b5e3c116cd8f6e3e7861924f6d559
Taegeon Go <tae.gun7784@gmail.com>
2021-06-17 18:45:03 +0900

    chore: modify metro config for svg & module resolver

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,31 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      extraNodeModules: {
+        assets: path.resolve(__dirname, 'assets'),
+        utils: path.resolve(__dirname, 'utils'),
+        screens: path.resolve(__dirname, 'screens'),
+        components: path.resolve(__dirname, 'components'),
+        api: path.resolve(__dirname, 'api'),
+        store: path.resolve(__dirname, 'store'),
       },
-    }),
-  },
-};
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
