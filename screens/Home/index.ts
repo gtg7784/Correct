24615db6da071b455efb21f02601038c7d35cd4f
Taegeon Go <tae.gun7784@gmail.com>
2021-06-19 02:16:52 +0900

    chore(screen): add home index file

--- screens/Home/index.ts
+++ screens/Home/index.ts
@@ -1,3 +1,11 @@
 import HomeScreen from './HomeScreen';
+import SelectQuestionCheckNumberScreen from './SelectQuestionCheckNumberScreen';
+import WriteAnswerCheckScreen from './WriteAnswerCheckScreen';
+import CheckAnswerScreen from './CheckAnswerScreen';
 
-export {HomeScreen};
+export {
+  HomeScreen,
+  SelectQuestionCheckNumberScreen,
+  WriteAnswerCheckScreen,
+  CheckAnswerScreen,
+};
