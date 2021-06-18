c9aa4bed246ac4274f7e45854249ca102c4506b4
Taegeon Go <tae.gun7784@gmail.com>
2021-06-19 02:17:01 +0900

    chore(screen): add setting index file

--- screens/Setting/index.ts
+++ screens/Setting/index.ts
@@ -1,3 +1,13 @@
 import SettingScreen from './SettingScreen';
+import ProfileScreen from './ProfileScreen';
+import NoticeScreen from './NoticeScreen';
+import NoticeDetailScreen from './NoticeDetailScreen';
+import ReportScreen from './ReportScreen';
 
-export {SettingScreen};
+export {
+  SettingScreen,
+  ProfileScreen,
+  NoticeScreen,
+  NoticeDetailScreen,
+  ReportScreen,
+};
