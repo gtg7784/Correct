820785688c0441ccb0e9a4ba90fb2050426c72dc
Taegeon Go <tae.gun7784@gmail.com>
2021-06-19 02:05:32 +0900

    chore: update workbook type declaration

--- @types/workbook.d.ts
+++ @types/workbook.d.ts
@@ -4,12 +4,26 @@ declare global {
   export interface IWorkbook {
     id: number;
     name: string;
-    bockcover_url: string;
-    answers: null | IAnswer[];
-    subject: string;
+    bookcover_url: string;
+    answers: null | IAnswer;
+    subject: SubjectType;
   }
 
   export interface IAnswer {
     [key: number]: number | string;
+  }
+
+  export type SubjectType = '국어' | '수학' | '사회' | '과학' | '영어' | '기타';
+
+  export type SubjectEnglishType =
+    | 'korean'
+    | 'math'
+    | 'society'
+    | 'science'
+    | 'english'
+    | 'etc';
+
+  export interface ISortedWorkbooks {
+    [key: string]: IWorkbook[];
   }
 }
